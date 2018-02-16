862ccbd288e1134ff1d25c980ab02293ab67cd29
Julio Graham <julioagraham@gmail.com>
2018-02-16 15:30:21 -0500

    Validation

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const accountSid = 'AC0bb2ca40be130cb4ca0bc569eb2cdcf7';
 const authToken = 'c57c50fe645b9dc00835290c44bcfc87';
 const client = new twilio(accountSid, authToken);
 
+let valid = /^(?:[1-9]|0[1-9]|10)$/
+
 let MessageSchema = new mongoose.Schema({
     phoneNumber: String,
     productQual: String,
@@ -35,16 +37,33 @@ app.post('/sms', (req, res) => {
     Message.find({phoneNumber: req.body.From}, (err, message) => {
         if (message.length !== 0) {
           if (!message[0].productQual && !message[0].custService) {
-            Message.findByIdAndUpdate(message[0]._id, {"$set": {"productQual": body}}, {"new": true, "upsert": true}, () => {
+            if (valid.test(body)) {
+                Message.findByIdAndUpdate(message[0]._id, {"$set": {"productQual": body}}, {"new": true, "upsert": true}, () => {
+                    client.messages.create({
+                        to: `${from}`,
+                        from: `${to}`,
+                        body: 'How would you rate the customer service on a scale of 1-10?'
+                    })
+    
+                    res.end();
+                })
+            } else {
                 client.messages.create({
                     to: `${from}`,
                     from: `${to}`,
-                    body: 'How would you rate the customer service on a scale of 1-10?'
+                    body: 'Please submit a number between 1-10.'
                 })
-
-                res.end();
-            })
+                Message.findByIdAndUpdate(message[0]._id, {"$set": {"productQual": body}}, {"new": true, "upsert": true}, () => {
+                    client.messages.create({
+                        to: `${from}`,
+                        from: `${to}`,
+                        body: 'How would you rate the product quality on a scale of 1-10?'
+                    })
+                })
+            }
+            
           } else if (!message[0].custService) {
+            if (valid.test(body)) {
             Message.findByIdAndUpdate(message[0]._id, {"$set": {"custService": body}}, {"new": true, "upsert": true}, () => {
                   client.messages.create({
                       to: `${from}`,
@@ -52,7 +71,21 @@ app.post('/sms', (req, res) => {
                       body: 'Thank you for your time. We hope to see you soon!'
                   })
               })
+          } else {
+            client.messages.create({
+                to: `${from}`,
+                from: `${to}`,
+                body: 'Please submit a number between 1-10.'
+            })
+            Message.findByIdAndUpdate(message[0]._id, {"$set": {"custService": body}}, {"new": true, "upsert": true}, () => {
+                client.messages.create({
+                    to: `${from}`,
+                    from: `${to}`,
+                    body: 'How would you rate the customer service on a scale of 1-10?'
+                })
+            })
           }
+        }
         } else {
             if (body === 'Survey' || 'survey') {
               let newMessage = new Message();
